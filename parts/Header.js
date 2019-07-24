9435af06af76ac0e42c9991f9c66c231ac7f271a
Grant Custer <grantcuster@gmail.com>
2019-07-23 21:51:52 -0400

    bigger link

--- parts/Header.js
+++ parts/Header.js
@@ -18,58 +18,19 @@ class Header extends React.Component {
         <div
           className={!is_front ? '' : 'no-opacity-hover'}
           style={{
-            padding: grem / 2,
             display: 'block',
             textDecoration: 'none',
             fontWeight: 700,
             display: 'flex',
           }}
         >
-          <div
-            style={{
-              width: grem,
-              height: grem,
-              marginRight: grem / 2,
-              position: 'relative',
-              display: 'none',
-            }}
-          >
-            <div
-              style={{
-                position: 'absolute',
-                left: 0,
-                top: 0,
-                width: grem / 2,
-                height: grem / 2,
-                background: analyze ? blue : 'white',
-              }}
-            />
-            <div
-              style={{
-                position: 'absolute',
-                left: grem / 2,
-                top: 0,
-                width: grem / 2,
-                height: grem / 2,
-                background: analyze ? red : 'white',
-              }}
-            />
-            <div
-              style={{
-                position: 'absolute',
-                left: 0,
-                top: grem / 2,
-                width: grem / 2,
-                height: grem / 2,
-                background: show_accuracy ? '#ddd' : 'white',
-              }}
-            />
-          </div>
           {is_front ? (
-            'Textflix'
+            <div style={{ padding: grem / 2 }}>Textflix</div>
           ) : (
             <Link href="/">
-              <a style={{ textDecoration: 'none' }}>Textflix</a>
+              <a style={{ textDecoration: 'none', padding: grem / 2 }}>
+                Textflix
+              </a>
             </Link>
           )}
         </div>
